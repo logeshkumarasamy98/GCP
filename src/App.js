3f27fc32211257fa58b51cac4f856bc6dd0c4048
logesh logesh <logeshlog420@gmail.com>
2023-12-20 18:28:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// app.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+document.addEventListener("DOMContentLoaded", function () {
+  // Function to generate a random hex color
+  function getRandomColor() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 
-export default App;
+  // Function to change the background color
+  function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+  }
+
+  // Create a button and add event listener
+  const button = document.createElement("button");
+  button.textContent = "Change Color";
+  button.addEventListener("click", changeBackgroundColor);
+
+  // Append the button to the body
+  document.body.appendChild(button);
+});
